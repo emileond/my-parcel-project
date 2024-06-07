801a1ca97d7a4a39c15bbbe91b9340d8a3432464
Emilio Sanchez <emilio@Emilios-MBP.localdomain>
2024-06-06 21:29:44 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// index.js
+async function loadTemplate(url) {
+  const response = await fetch(url)
+  const text = await response.text()
+  const template = document.createElement('div')
+  template.innerHTML = text
+  document.body.appendChild(template)
+}
+
+async function init() {
+  await loadTemplate('./components/button.html')
+  //   await loadTemplate('./templates/card.html')
+
+  // Example of using the templates
+  const buttonTemplate = document.getElementById('button-template')
+  //   const cardTemplate = document.getElementById('card-template')
+
+  const buttonClone = buttonTemplate.content.cloneNode(true)
+  //   const cardClone = cardTemplate.content.cloneNode(true)
+
+  document.getElementById('app').appendChild(buttonClone)
+  //   document.getElementById('app').appendChild(cardClone)
+}
+
+init()
